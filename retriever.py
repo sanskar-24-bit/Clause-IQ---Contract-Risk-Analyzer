5bbfbc518992bc2249899345893c9f77ee161e79
Sanskar Shinde <sanskarshinde07s@gmail.com>
2026-09-25 22:22:02 +0530

    Delete retriever.py

--- retriever.py
+++
@@ -1,6 +0,0 @@
-from vector_store import load_vector_store
-from config import VECTOR_STORE_DIR
-
-def get_retriever( k : int = 4, persist_dir : str = VECTOR_STORE_DIR):
-    vector_store = load_vector_store(persist_dir)
-    return vector_store.as_retriever(search_kwargs = {"k":k})
